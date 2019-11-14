c4f78694d096a924a755f9ed7c10e0bc66f84020
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-11-14 11:17:43 -0800

    mvp + stretch goals hit

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ Airplane.prototype.land = function () {
 
 /*
   TASK 1
-    - Write a Person Constructor that initializes `name` and `age` from arguments.
-    - All instances of Person should initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
+    x Write a Person Constructor that initializes `name` and `age` from arguments.
+    x All instances of Person should initialize with an empty `stomach` array.
+    x Give instances of Person the ability to `.eat("someFood")`:
         + When eating an edible, it should be pushed into the `stomach`.
         + The `eat` method should have no effect if there are 10 items in the `stomach`.
     - Give instances of Person the ability to `.poop()`:
@@ -39,28 +39,61 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(food){
+  if(this.stomach.length === 10){
+    //do nothing
+  }else{
+    this.stomach.push(food)
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = []
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
 }
 
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
+    x Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    x All instances built with Car:
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+    x Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    x STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+    x STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+};
 
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons
+};
+
+Car.prototype.drive = function (distance){
+  this.odometer = this.odometer+distance
+  if(this.milesPerGallon * this.tank < distance){
+    this.odometer =  this.milesPerGallon * this.tank
+    this.tank = 0
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }else{
+    this.tank = this.tank - (distance/this.milesPerGallon)
+  }
 }
-
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,18 +101,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, favoriteToy)
+  this.name = name
+  this.age = age
+  this.favoriteToy = favoriteToy
 }
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`
+}
+// use object.create here
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Global Object Binding: when used in global scope, the browser window is the object
+  2. Implicit Binding: binding this to an object when using a method to call the object
+  3. New Binding: used in constructor functions
+  4. Explicit Binding: used with call and apply
 */
 
 
